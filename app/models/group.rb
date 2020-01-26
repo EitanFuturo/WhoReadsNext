c298847a81410886383e7383377eea1bec9e78c1
EitanFuturo <llovia@gmail.com>
2020-01-26 19:02:05 -0300

    Added teacher and group to index and show views for models Group and Student respectively.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
-  has_and_belongs_to_many :teachers
+  has_and_belongs_to_many :teachers, touch: true
   has_and_belongs_to_many :students
+
+  def teacher_email
+    teachers.order(:updated_at).last.try(:email) || 'Unknown'
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ApplicationRecord
   has_and_belongs_to_many :groups
+
+  def group_name
+    groups.order(:updated_at).last.try(:name) || 'Unknown'
+  end
 end
